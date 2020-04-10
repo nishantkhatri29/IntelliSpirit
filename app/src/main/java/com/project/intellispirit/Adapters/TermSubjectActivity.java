a9b794d83b171e571b4687d519b5a42c04ae0740
himanshik960 <46323269+himanshik960@users.noreply.github.com>
2020-04-10 22:00:27 +0530

    Deleted Extra Toolbar xml and hided the profile icon while viewing subject list

--- app/src/main/java/com/project/intellispirit/Adapters/TermSubjectActivity.java
+++ app/src/main/java/com/project/intellispirit/Adapters/TermSubjectActivity.java
@@ -1,8 +1,11 @@
 package com.project.intellispirit.Adapters;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.project.intellispirit.R;
@@ -11,11 +14,21 @@ public class TermSubjectActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private Term1Subject1ItemAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    Toolbar toolbar;
+    ImageView imageView_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_subject);
+
+        imageView_profile=findViewById(R.id.profile_image);
+
+        toolbar = findViewById(R.id.mytool);
+        setSupportActionBar(toolbar);
+
+        imageView_profile.setVisibility(View.GONE);
+
         int pos = Integer.parseInt(getIntent().getStringExtra("position"));
 
 
