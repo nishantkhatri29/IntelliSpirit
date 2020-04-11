fae86805dafa930287a855ee31ae803f89dd0046
Lakshay Baweja <lakshay17csu0993@ncuindia.edu>
2020-04-11 16:32:18 +0530

    Animations on Splash Screen

--- app/src/main/java/com/project/intellispirit/SplashScreen/SplashActivity.java
+++ app/src/main/java/com/project/intellispirit/SplashScreen/SplashActivity.java
@@ -1,19 +1,26 @@
-package com.project.intellispirit;
+package com.project.intellispirit.SplashScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.project.intellispirit.LoginPage.LoginActivity;
+import com.project.intellispirit.R;
 
 public class SplashActivity extends AppCompatActivity {
-
+ImageView imageView_logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        imageView_logo=findViewById(R.id.imageView_logo);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.splash_animation);
+        imageView_logo.setAnimation(animation);
         new Handler().postDelayed(new Runnable() {
 
 
@@ -23,7 +30,8 @@ public class SplashActivity extends AppCompatActivity {
                 Intent i = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
-        }, 2000);
+        }, 3000);
     }
 }
