0f510c21c919813065d90d06acbf51ebb228bda7
Lakshay Baweja <lakshay17csu0993@ncuindia.edu>
2020-04-27 15:06:19 +0530

    changes made in teacher activity

--- app/src/main/java/com/project/intellispirit/UserHomePages/Teacher_Activity.java
+++ app/src/main/java/com/project/intellispirit/UserHomePages/Teacher_Activity.java
@@ -62,15 +62,7 @@ public class Teacher_Activity extends AppCompatActivity {
 
         });
         webView_teacher.getSettings().setJavaScriptEnabled(true);
-        webView_teacher.getSettings().setLoadWithOverviewMode(true);
-        webView_teacher.getSettings().setUseWideViewPort(true);
 
-        webView_teacher.getSettings().setSupportZoom(true);
-        webView_teacher.getSettings().setBuiltInZoomControls(true);
-        webView_teacher.getSettings().setDisplayZoomControls(false);
-
-        webView_teacher.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        webView_teacher.setScrollbarFadingEnabled(false);
         webView_teacher.loadUrl("https://intellispirit0.herokuapp.com/"+teacher_id);
 
 
@@ -78,11 +70,6 @@ public class Teacher_Activity extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finishAffinity();
-    }
 
     public void logout() {
         SharedPreferences sharedPreferences=getSharedPreferences("LogIn",MODE_PRIVATE);
@@ -95,22 +82,12 @@ public class Teacher_Activity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            switch (keyCode) {
-                case KeyEvent.KEYCODE_BACK:
-                    if (webView_teacher.canGoBack()) {
-                        webView_teacher.goBack();
-                    } else {
-                        finish();
-
-                    }
-                    return true;
-            }
-
-        }
-        return super.onKeyDown(keyCode, event);
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
     }
+
 
 }
